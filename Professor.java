7136394b5157a5a0581db5df4c8b143b54cc923a
Hugo Brendow <hugobrendow97@gmail.com>
2022-11-07 21:05:06 -0300

    Adicionando funcionalidade

--- Professor.java
+++ Professor.java
@@ -6,6 +6,12 @@ public class Professor {
         System.out.println("Digite o nome do professor: ");
         String nome = scanner.nextLine();
 
+        System.out.println("Digite a idade do professor: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite o cpf do professor: ");
+        String cpf = scanner.nextLine();
+
         System.out.printf("Bem-vindo %s", nome);
     }
 }
